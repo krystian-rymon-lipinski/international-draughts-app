1b7414f7ffe481e03e1ba420133e962c876d43e0
krystian-rymon-lipinski <rymon9696@gmail.com>
2020-08-18 21:08:19 +0200

    Add margins to main frame

--- src/main/java/com/krystianrymonlipinski/main_frame/MainFrame.java
+++ src/main/java/com/krystianrymonlipinski/main_frame/MainFrame.java
@@ -8,10 +8,22 @@ import com.krystianrymonlipinski.tree.model.Tree;
 import draughts.library.managers.BoardManager;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame {
 
+    private static final int MAIN_FRAME_WIDTH = 1000;
+    private static final int MAIN_FRAME_HEIGHT = 650;
+
+    private static final int CONTROL_PANEL_WIDTH = 300;
+    private static final int CONTROL_PANEL_HEIGHT = 700;
+
+    private static final int MARGIN_WIDTH = 75;
+    private static final int MARGIN_HEIGHT = 75;
+
     JFrame mainFrame;
+    BoardPanel boardPanel;
+    JPanel controlPanel;
     JButton newGameButton;
     JButton makeMoveButton;
     JButton loadMoveButton;
@@ -23,8 +35,57 @@ public class MainFrame {
 
     private void createFrame() {
         mainFrame = new JFrame();
-        mainFrame.setSize(900, 700);
+        mainFrame.setLayout(null);
+
+        JPanel topMargin = new JPanel();
+        topMargin.setBounds(0, 0, MAIN_FRAME_WIDTH, MARGIN_HEIGHT);
+        topMargin.setBackground(Color.WHITE);
+
+        JPanel westMargin = new JPanel();
+        westMargin.setBounds(0, 0, MARGIN_WIDTH, MAIN_FRAME_HEIGHT);
+        westMargin.setBackground(Color.WHITE);
+
+        JPanel eastMargin = new JPanel();
+        eastMargin.setBounds( (MAIN_FRAME_WIDTH - MARGIN_WIDTH), 0, MARGIN_WIDTH, MAIN_FRAME_HEIGHT);
+        eastMargin.setBackground(Color.WHITE);
+
+        JPanel bottomMargin = new JPanel();
+        bottomMargin.setBounds(0, (MAIN_FRAME_HEIGHT - MARGIN_HEIGHT), MAIN_FRAME_WIDTH, MARGIN_HEIGHT);
+        bottomMargin.setBackground(Color.WHITE);
+
+        JPanel contentPanel = new JPanel();
+        contentPanel.setBounds(MARGIN_WIDTH, MARGIN_HEIGHT,
+                               MAIN_FRAME_WIDTH - MARGIN_WIDTH,
+                               MAIN_FRAME_HEIGHT - MARGIN_HEIGHT);
+        contentPanel.setBackground(Color.orange);
+
+
+
+        mainFrame.getContentPane().add(topMargin);
+        mainFrame.getContentPane().add(westMargin);
+        mainFrame.getContentPane().add(eastMargin);
+        mainFrame.getContentPane().add(bottomMargin);
+        mainFrame.getContentPane().add(contentPanel);
+
+        //mainFrame.getContentPane().add(createBoardPanel());
+        //mainFrame.getContentPane().add(createControlPanel());
+
+        mainFrame.setSize(MAIN_FRAME_WIDTH, MAIN_FRAME_HEIGHT);
         mainFrame.setVisible(true);
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //mainFrame.repaint();
+
+    }
+
+    private BoardPanel createBoardPanel() {
+        boardPanel = new BoardPanel();
+        return boardPanel;
+    }
+
+    private JPanel createControlPanel() {
+        controlPanel = new JPanel();
+        controlPanel.setSize(CONTROL_PANEL_WIDTH, CONTROL_PANEL_HEIGHT);
+        controlPanel.setBackground(Color.RED);
+        return controlPanel;
     }
 }
