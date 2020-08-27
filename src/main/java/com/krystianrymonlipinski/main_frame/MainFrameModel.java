fa0d81a54160543c01313649764f4c806ef3abd1
krystian-rymon-lipinski <rymon9696@gmail.com>
2020-08-27 19:31:33 +0200

    Update board state after loading move

--- src/main/java/com/krystianrymonlipinski/main_frame/MainFrameModel.java
+++ src/main/java/com/krystianrymonlipinski/main_frame/MainFrameModel.java
@@ -10,12 +10,14 @@ import draughts.library.movemodel.Move;
 public class MainFrameModel {
 
     private GameEngine gameEngine;
+    private Move<? extends Hop> loadedMove;
     private int moveNumber;
 
     private MoveFileManager moveFileManager;
 
     public MainFrameModel() {
         gameEngine = new GameEngine();
+        loadedMove = null;
     }
 
     public Tile[][] startGame() {
@@ -29,15 +31,23 @@ public class MainFrameModel {
         MoveData moveData = moveFileManager.loadMoveData();
         boolean isLegal;
         try {
-            gameEngine.checkIfMoveIsCorrect(moveData.getSource(),
-                                            moveData.getDestination(),
-                                            moveData.getTakenPawns());
+            loadedMove = gameEngine.checkIfMoveIsCorrect(moveData.getSource(),
+                                                         moveData.getDestination(),
+                                                         moveData.getTakenPawns());
             isLegal = true;
         } catch (WrongMoveException | GameAlreadyEndedException e) {
             e.printStackTrace();
             isLegal = false;
         }
-        moveFileManager.writeMoveResult(isLegal);
+        //moveFileManager.writeMoveResult(isLegal);
         return isLegal;
+    }
+
+    public Tile[][] updateBoard() {
+        gameEngine.getBoardManager().makeWholeMove(loadedMove);
+        gameEngine.finishMove(loadedMove);
+        gameEngine.prepareMove(gameEngine.getIsWhiteToMove());
+
+        return gameEngine.getBoardManager().getBoard();
     }
 }
